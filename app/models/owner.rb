cf0ef4fe6706b564d7049477136c76bc85ceee7b
Cecily Walker <cecily.walker@gmail.com>
2015-11-15 17:44:57 +0000

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,21 @@
+class Owner
+    
+        def name
+            name = 'Cecily Walker'
+        end
+        
+        def birthdate
+            birthdate = Date.new(1968, 9, 18)
+        end
+        
+    def countdown
+        today = Date.today
+        birthday = Date.new(today.year, birthdate.month, birthdate.day)
+        if birthday > today
+        countdown = (birthday - today).to_i
+    else
+        countdown = (birthday.next_year - today).to_i
+    end
+  end
+
+end
